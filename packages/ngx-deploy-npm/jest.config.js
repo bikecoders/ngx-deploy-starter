d4cdc93de56dedf15ae4e42b650696b58a1072f9
Diego Julião <dianjuar@gmail.com>
2022-10-06 22:40:15 -0700

    test: fix jest depreacted option (#378)

--- packages/ngx-deploy-npm/jest.config.js
+++ packages/ngx-deploy-npm/jest.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   displayName: 'ngx-deploy-npm',
   preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest',
+    '^.+\\.[tj]sx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/packages/ngx-deploy-npm',
