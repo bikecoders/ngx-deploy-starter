dc7aac82af248fc47b9d0f097338d8c900dfa7ff
dianjuar <dianjuar@gmail.com>
2022-10-07 00:20:47 -0500

    test: fix jest depreacted option

--- e2e/ngx-deploy-npm-e2e/jest.config.js
+++ e2e/ngx-deploy-npm-e2e/jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   displayName: 'ngx-deploy-npm-e2e',
   preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/e2e/ngx-deploy-npm-e2e',
