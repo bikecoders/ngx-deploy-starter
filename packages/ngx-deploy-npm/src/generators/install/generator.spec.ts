32ecbab1c64abbc9a6ee790265ac9bb4cf027eb4
dianjuar <dianjuar@gmail.com>
2022-12-03 13:19:21 -0500

    build: migrate to nx 15.2.4

--- packages/ngx-deploy-npm/src/generators/install/generator.spec.ts
+++ packages/ngx-deploy-npm/src/generators/install/generator.spec.ts
@@ -1,4 +1,4 @@
-import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
+import { createTreeWithEmptyV1Workspace } from '@nrwl/devkit/testing';
 import {
   Tree,
   addProjectConfiguration,
@@ -44,7 +44,7 @@ describe('install/ng-add generator', () => {
   beforeEach(() => {
     rawOptions = {};
 
-    appTree = createTreeWithEmptyWorkspace();
+    appTree = createTreeWithEmptyV1Workspace();
   });
 
   beforeEach(() => {
