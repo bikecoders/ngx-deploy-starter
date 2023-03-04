1af1a413257b5c5bb9df2a75dea15e7349f966ec
dianjuar <dianjuar@gmail.com>
2023-03-04 12:41:24 -0500

    chore: create nx root level script to create nx workspace sandbox

--- tools/create-nx-workspace.sh
+++ tools/create-nx-workspace.sh
@@ -2,27 +2,32 @@
 
 set -e
 
+version="${1:-latest}"
+
 mkdir -p tmp
 cd tmp
 
 # Delete the previous workspace
 rm -Rif nx-workspace
 
-npx create-nx-workspace --version
+npx --yes create-nx-workspace@$version --version
 
-echo "Create Nx Workspace"
-npx create-nx-workspace --packageManager yarn --name nx-workspace --preset empty  --nxCloud false
+echo "----- Create Nx Workspace -----"
+npx create-nx-workspace@$version --name nx-workspace --preset empty --nxCloud false
 cd nx-workspace
 
-echo "Generate lib"
-yarn add -D @nrwl/node
+echo "NX_WORKSPACE_ROOT_PATH=$(pwd)" >.env
+
+echo "----- Generate lib -----"
+npm add -D @nrwl/node@$version
 npx nx generate @nrwl/node:lib --name nx-lib --publishable --importPath nx-lib
 
-echo "Link ngx-deploy-npm"
-yarn link ngx-deploy-npm
+echo "----- Link ngx-deploy-npm -----"
+npx yalc add ngx-deploy-npm
+npm i
 
-echo "Add ngx-deploy-npm to the workspace"
+echo "----- Add ngx-deploy-npm to the workspace -----"
 npx nx generate ngx-deploy-npm:install
 
-echo "Test the build"
+echo "----- Test the build -----"
 npx nx deploy nx-lib --dry-run
