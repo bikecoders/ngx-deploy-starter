f37c5b458ca529b87b88bae7097410b0d7b0b4d4
dianjuar <dianjuar@gmail.com>
2023-03-25 19:29:14 -0500

    ci: migrate from yarn to npm

--- e2e/ngx-deploy-npm-e2e/utils/utils-ngx-deploy-npm.ts
+++ e2e/ngx-deploy-npm-e2e/utils/utils-ngx-deploy-npm.ts
@@ -10,12 +10,12 @@ export function initNgxDeployNPMProject() {
     ensureNxProject('ngx-deploy-npm', 'dist/packages/ngx-deploy-npm');
 
     runNxCommand('generate ngx-deploy-npm:init');
+    runPackageManagerInstall();
   }, 120000);
 }
 
 export function installNgxDeployNPMProject(options: string = '') {
   beforeEach(() => {
     runNxCommand(`generate ngx-deploy-npm:install ${options}`);
-    runPackageManagerInstall();
   }, 5000);
 }
