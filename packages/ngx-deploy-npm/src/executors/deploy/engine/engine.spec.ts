9841da223197c0558f7b9fd4c97dddf758e2703e
Diego Juliao <dianjuar@gmail.com>
2024-08-12 15:45:28 -0500

    test: migrate engine test to sifers

--- packages/ngx-deploy-npm/src/executors/deploy/engine/engine.spec.ts
+++ packages/ngx-deploy-npm/src/executors/deploy/engine/engine.spec.ts
@@ -5,37 +5,40 @@ import * as spawn from '../utils/spawn-async';
 import * as setPackage from '../utils/set-package-version';
 import { mockProjectDist, mockProjectRoot } from '../../../__mocks__/mocks';
 
-// TODO Migrate to SIFERS approach
 describe('engine', () => {
-  let dir: string;
-  let distFolderPath: string;
-  let options: DeployExecutorOptions;
+  const defaultOption: Readonly<Omit<DeployExecutorOptions, 'distFolderPath'>> =
+    Object.freeze({
+      access: npmAccess.public,
+    });
+  const setup = ({
+    options = defaultOption,
+    rootProject = mockProjectRoot,
+    distFolderPath = mockProjectDist(),
+    spawnAsyncReturnValue = () => Promise.resolve(),
+  }: {
+    rootProject?: string;
+    distFolderPath?: string;
+    spawnAsyncReturnValue?: () => Promise<void>;
+    options?: Omit<DeployExecutorOptions, 'distFolderPath'>;
+  }) => {
+    const fullOptions: DeployExecutorOptions = {
+      ...options,
+      distFolderPath,
+    };
+    jest.spyOn(spawn, 'spawnAsync').mockImplementation(spawnAsyncReturnValue);
+
+    return {
+      absoluteDistFolderPath: `${rootProject}/${distFolderPath}`,
+      options: fullOptions,
+    };
+  };
 
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  // Spies
-  beforeEach(() => {
-    jest.spyOn(spawn, 'spawnAsync').mockImplementation(() => Promise.resolve());
-  });
-
-  // Data
-  beforeEach(() => {
-    dir = mockProjectRoot;
-    distFolderPath = mockProjectDist();
-  });
-
   it('should call NPM Publish with the right options', async () => {
-    options = {
-      distFolderPath,
-      access: npmAccess.restricted,
-      tag: 'next',
-      otp: 'someValue',
-      registry: 'http://localhost:4873',
-      dryRun: true,
-    };
-    const optionsArray = [
+    const expectedOptionsArray = [
       '--access',
       npmAccess.restricted,
       '--tag',
@@ -47,97 +50,80 @@ describe('engine', () => {
       '--registry',
       'http://localhost:4873',
     ];
+    const { absoluteDistFolderPath, options } = setup({
+      options: {
+        access: npmAccess.restricted,
+        tag: 'next',
+        otp: 'someValue',
+        registry: 'http://localhost:4873',
+        dryRun: true,
+      },
+    });
 
-    await engine.run(dir, options);
+    await engine.run(absoluteDistFolderPath, options);
 
     expect(spawn.spawnAsync).toHaveBeenCalledWith('npm', [
       'publish',
-      dir,
-      ...optionsArray,
+      absoluteDistFolderPath,
+      ...expectedOptionsArray,
     ]);
   });
 
   it('should indicate that an error occurred when there is an error publishing the package', async () => {
-    const customErr = 'custom err';
-    jest
-      .spyOn(spawn, 'spawnAsync')
-      .mockImplementation(() => Promise.reject(new Error(customErr)));
-
-    await expect(() => engine.run(dir, options)).rejects.toThrow();
-  });
-
-  describe('Options Management', () => {
-    it('should set the default options', async () => {
-      const options: DeployExecutorOptions = {
-        distFolderPath,
-        access: npmAccess.public,
-      };
-      const optionsArray = ['--access', npmAccess.public];
-
-      await engine.run(dir, options);
-
-      expect(spawn.spawnAsync).toHaveBeenCalledWith('npm', [
-        'publish',
-        dir,
-        ...optionsArray,
-      ]);
+    const { absoluteDistFolderPath, options } = setup({
+      spawnAsyncReturnValue: () => Promise.reject(new Error('custom error')),
     });
 
-    it('should overwrite the default option access', async () => {
-      const options = {
-        distFolderPath: 'dist/libs/project',
-        tag: 'random-tag',
-        access: npmAccess.restricted,
-      };
-      const optionsArray = [
-        '--access',
-        npmAccess.restricted,
-        '--tag',
-        'random-tag',
-      ];
-
-      await engine.run(dir, options);
-
-      expect(spawn.spawnAsync).toHaveBeenCalledWith('npm', [
-        'publish',
-        dir,
-        ...optionsArray,
-      ]);
-    });
+    await expect(() =>
+      engine.run(absoluteDistFolderPath, options)
+    ).rejects.toThrow();
   });
 
   describe('Package.json Feature', () => {
-    let version: string;
-    let options: DeployExecutorOptions;
-
-    // Spies
-    beforeEach(() => {
+    const pJsonSetup = ({
+      version = '1.0.1-next0',
+      setPackageReturnValue = Promise.resolve(),
+      ...originalSetupOptions
+    }: {
+      version?: string;
+      setPackageReturnValue?: Promise<void>;
+    } & Parameters<typeof setup>[0]) => {
       jest
         .spyOn(setPackage, 'setPackageVersion')
-        .mockImplementation(() => Promise.resolve());
-    });
+        .mockImplementation(() => setPackageReturnValue);
 
-    // Data
-    beforeEach(() => {
-      version = '1.0.1-next0';
+      if (!originalSetupOptions.options) {
+        originalSetupOptions.options = { ...defaultOption };
+      }
 
-      options = {
-        distFolderPath,
-        packageVersion: version,
-        access: 'public',
+      originalSetupOptions.options.packageVersion = version;
+
+      return {
+        version,
+        ...setup(originalSetupOptions),
       };
-    });
+    };
 
     it('should write the version of the sent on the package.json', async () => {
-      await engine.run(dir, options);
+      const { absoluteDistFolderPath, version, options } = pJsonSetup({});
 
-      expect(setPackage.setPackageVersion).toHaveBeenCalledWith(dir, version);
+      await engine.run(absoluteDistFolderPath, options);
+
+      expect(setPackage.setPackageVersion).toHaveBeenCalledWith(
+        absoluteDistFolderPath,
+        version
+      );
     });
 
     it('should not write the version of the sent on the package.json if is on dry-run mode', async () => {
-      options.dryRun = true;
+      const { absoluteDistFolderPath, options } = pJsonSetup({
+        options: {
+          access: npmAccess.public,
+          dryRun: true,
+        },
+      });
 
-      await engine.run(dir, options);
+      await engine.run(absoluteDistFolderPath, options);
 
       expect(setPackage.setPackageVersion).not.toHaveBeenCalled();
     });
--- packages/ngx-deploy-npm/src/executors/deploy/utils/index.ts
+++ packages/ngx-deploy-npm/src/executors/deploy/utils/index.ts
@@ -1,5 +1,3 @@
-export * from './default-options';
 export * from './interfaces';
-export * from './set-default-options';
 export * from './set-package-version';
 export * from './spawn-async';
